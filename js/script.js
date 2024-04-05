c0733b6dc2eefcbfd904228a5691700246fcfc48
ANIKET KUMAR <147272365+Aniket322@users.noreply.github.com>
2024-04-04 20:34:27 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// console.log('Hello ');
+
+// var messsage = 'hello world';
+
+// var age = 18;
+// var age = "done";
+
+// console.log(messsage);
+
+// var messsage = "hello";
+// console.log(messsage);
+
+//  var message = "else";
+//  console.log(message)
+
+// let 
+//console.log(message);
+//let message="hello";
+//console.log(message);
+
+// const
+
+//  const message;
+// const message ;
+//  message ='else' ;
+// console.log(message);
+
+
+// const str = 'khan';
+// const username = 'my name is :';
+// const ans =  `${username} ${str}` ;
+// console.log(ans);
+
+// function print (str ='nothing is here'){
+//     console.log(str);
+// }
+
+// print();
+
+
+// function sum (a,b){
+//     if(a && b){
+
+//         console.log(a+b);
+//     }
+//     else{
+//         console.log(a);
+//     }
+
+// }
+//   sum(20);
+// 
+
+// let a = 2;
+
+// switch(a){
+
+//     case 1 : {
+//         console.log('one');
+//         break;
+//     }
+//     case 2 : {
+//         console.log('two');
+//         break;
+//     }
+//     case 3 : {
+//         console.log('three');
+//         break;
+//     }
+//     default : {
+//         console.log('not 1,2,3');
+       
+//     }
+
+// }
+
+// const obj = new Object();
+// obj [1] = 'one';
+// obj [12] = 'twelve';
+// obj ['name'] = 'anny';
+// console.log(obj);
+// // different ways to declared objects
+// const obj = {
+//     1: 'one',
+//     'name':'anny',
+//     12: 'twelve',
+// };
+
+// console.log(obj);
+
+ const obj = {
+
+'firstName' : 'Ajay',
+'lastName'  :  'singh',
+'age' : 20,
+'address' : 'Delhi'
+
+};
+
+// const fullname = obj.firstName+' '+obj.lastName;
+// console.log(fullname);
+
+const x = prompt('please tell the property to know');
+//console.log(x);
+console.log(obj(x));
